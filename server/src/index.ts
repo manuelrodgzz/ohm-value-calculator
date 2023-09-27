88eef714b75e0d3acb50d2f1d8e0e4c9e52eab86
Manuel Alejandro Rodriguez Gonzalez <manuelalejandrorodriguezgonzalez@Manuels-Mac-mini.local>
2023-09-27 11:16:23 -0600

    Move server code to src folder

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,8 @@
 import express from 'express'
-import path from 'path'
 
 const PORT = 3001
 
 const app = express()
-
-app.use(
-  express.static(
-    path.resolve(__dirname, '../client/dist')
-  )
-)
 
 app.get('/api', (req, res) => {
   res.send('Hello world')
