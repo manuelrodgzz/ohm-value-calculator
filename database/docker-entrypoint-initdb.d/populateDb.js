aead4c82eae4fe78e9d30e6849f2baf4f805bba8
Manuel Alejandro Rodriguez Gonzalez <manuelalejandrorodriguezgonzalez@Manuels-Mac-mini.local>
2023-09-30 13:56:22 -0600

    Create database package

---
+++ database/docker-entrypoint-initdb.d/populateDb.js
@@ -0,0 +1,120 @@
+const fs = require('fs')
+
+const DOCUMENTS = [
+  {
+    color: 'black',
+    significant: 0,
+    multiplier: 1
+  },
+  {
+    color: 'brown',
+    significant: 1,
+    multiplier: 10,
+    tolerancePercentage: 0.01
+  },
+  {
+    color: 'red',
+    significant: 2,
+    multiplier: 100,
+    tolerancePercentage: 0.02
+  },
+  {
+    color: 'orange',
+    significant: 3,
+    multiplier: 1000,
+    tolerancePercentage: 0.0005
+  },
+  {
+    color: 'yellow',
+    significant: 4,
+    tolerancePercentage: 0.0002,
+    multiplier: 10_000
+  },
+  {
+    color: 'green',
+    significant: 5,
+    multiplier: 100_000,
+    tolerancePercentage: 0.005
+  },
+  {
+    color: 'blue',
+    significant: 6,
+    multiplier: 1_000_000,
+    tolerancePercentage: 0.0025
+  },
+  {
+    color: 'violet',
+    significant: 7,
+    multiplier: 10_000_000,
+    tolerancePercentage: 0.001
+  },
+  {
+    color: 'grey',
+    significant: 8,
+    multiplier: 100_000_000,
+    tolerancePercentage: 0.0001
+  },
+  {
+    color: 'white',
+    significant: 9,
+    multiplier: 1_000_000_000
+  },
+  {
+    color: 'gold',
+    multiplier: 	0.1,
+    tolerancePercentage: 0.05
+  },
+  {
+    color: 'silver',
+    multiplier: 	0.01,
+    tolerancePercentage: 0.1
+  },
+  {
+    color: 'pink',
+    multiplier: 0.001
+  },
+  {
+    color: 'none',
+    tolerancePercentage: 0.2
+  }
+]
+
+const {
+  MONGO_CLIENT_USER_FILE,
+  MONGO_CLIENT_PASSWORD_FILE,
+  MONGO_INITDB_DATABASE
+} = process.env
+
+if (!MONGO_CLIENT_USER_FILE) {
+  throw new Error(`Missing env variable: MONGO_CLIENT_USER_FILE`)
+}
+
+if (!MONGO_CLIENT_PASSWORD_FILE) {
+  throw new Error(`Missing env variable: MONGO_CLIENT_PASSWORD_FILE`)
+}
+
+if (!MONGO_INITDB_DATABASE) {
+  throw new Error(`Missing env variable: MONGO_INITDB_DATABASE`)
+}
+
+const CLIENT_USER = fs.readFileSync(process.env.MONGO_CLIENT_USER_FILE).toString()
+const CLIENT_PASSWORD = fs.readFileSync(process.env.MONGO_CLIENT_PASSWORD_FILE).toString()
+
+db.getSiblingDB('admin').auth(
+  process.env.MONGO_INITDB_ROOT_USERNAME,
+  process.env.MONGO_INITDB_ROOT_PASSWORD
+)
+
+const res = db.createUser(
+  {
+    user: CLIENT_USER,
+    pwd: CLIENT_PASSWORD,
+    roles: [{ role: 'read', db: process.env.MONGO_INITDB_DATABASE }]
+  },
+)
+
+db.createCollection('colorCode')
+
+console.log('===== Populating DB ====')
+db.colorCode.insertMany(DOCUMENTS)
+console.log('===== DB population finished ====')
