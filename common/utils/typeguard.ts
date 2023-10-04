fbb0e4a1747e97b51843e2548f2c27bc674e83da
Manuel Alejandro Rodriguez Gonzalez <manuelalejandrorodriguezgonzalez@Manuels-Mac-mini.local>
2023-10-04 16:08:22 -0600

    Fix typeguard

--- common/utils/typeguard.ts
+++ common/utils/typeguard.ts
@@ -1,7 +1,9 @@
 export const typeguard = <T>(toEvaluate: any, propToCheck: keyof T): toEvaluate is T => {
   try {
     if (propToCheck in toEvaluate) return  true
-  } finally {
+
+    return false
+  } catch {
     return false
   }
 }
