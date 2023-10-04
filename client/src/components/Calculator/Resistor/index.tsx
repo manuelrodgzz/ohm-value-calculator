506f9a1c057d7799bfa499886a883e7eaf98ec37
Manuel Alejandro Rodriguez Gonzalez <manuelalejandrorodriguezgonzalez@Manuels-Mac-mini.local>
2023-10-04 16:15:10 -0600

    Fix overflow of bands on mobile

--- client/src/components/Calculator/Resistor/index.tsx
+++ client/src/components/Calculator/Resistor/index.tsx
@@ -4,6 +4,7 @@ import styled from '@emotion/styled'
 
 type BandProps = {
   color?: Color
+  letter: string
 }
 
 type ResistorProps = ResistorType
@@ -23,21 +24,28 @@ const ResistorWrapper = styled.div`
     border-bottom-right-radius: 12rem;
     justify-content: space-evenly;
     background-color: rgb(255, 222, 172);
+    overflow: hidden;
 
     .band {
       border: 1px solid black;
       border-top: none;
       border-bottom: none;
       width: 1rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-weight: 500;
     }
   }
 `
 
-const Band: FC<BandProps> = ({color = 'none'}) => (
+const Band: FC<BandProps> = ({color = 'none', letter}) => (
   <div
     style={{backgroundColor: color === 'none' ? 'transparent' : color}}
     className='band'
-  />
+  >
+    <span>{letter}</span>
+  </div>
 )
 
 const Resistor: FC<ResistorProps> = ({ bandA, bandB, bandC, bandD }) => {
@@ -45,10 +53,10 @@ const Resistor: FC<ResistorProps> = ({ bandA, bandB, bandC, bandD }) => {
   return (
     <ResistorWrapper>
       <div className='resistor'>
-        <Band color={ bandA }/>
-        <Band color={ bandB }/>
-        <Band color={ bandC }/>
-        <Band color={ bandD }/>
+        <Band color={ bandA } letter='A'/>
+        <Band color={ bandB } letter='B'/>
+        <Band color={ bandC } letter='C'/>
+        <Band color={ bandD } letter='D'/>
       </div>
     </ResistorWrapper>
   )
