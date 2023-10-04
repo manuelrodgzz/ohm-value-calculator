1d59a82ea2f542938d371dc468fd7d8d36f6e266
Manuel Alejandro Rodriguez Gonzalez <manuelalejandrorodriguezgonzalez@Manuels-Mac-mini.local>
2023-10-04 10:18:30 -0600

    Enable cors on server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import ohmRouter from './routes/ohm'
 import colorsRouter from './routes/colors'
+import cors from 'cors'
 
 const PORT = 3001
 
 export const app = express()
 
+app.use(cors())
 app.use('/api',ohmRouter, colorsRouter)
 
 app.listen(PORT, () => {
