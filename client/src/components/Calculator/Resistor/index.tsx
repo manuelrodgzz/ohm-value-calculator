4eaafe42c039ac9e622cfe993b3b80727aea0687
Manuel Alejandro Rodriguez Gonzalez <manuelalejandrorodriguezgonzalez@Manuels-Mac-mini.local>
2023-10-04 14:14:10 -0600

    Replace Resistor component styles with a @emotion styled component

--- client/src/components/Calculator/Resistor/index.tsx
+++ client/src/components/Calculator/Resistor/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
-import styles from './styles.module.css'
 import { Color, Resistor as ResistorType } from 'common'
+import styled from '@emotion/styled'
 
 type BandProps = {
   color?: Color
@@ -8,24 +8,53 @@ type BandProps = {
 
 type ResistorProps = ResistorType
 
+const ResistorWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+
+  .resistor {
+    border: 1px solid black;
+    display: flex;
+    height: 10rem;
+    width: 30rem;
+    border-top-left-radius: 12rem;
+    border-bottom-left-radius: 12rem;
+    border-top-right-radius: 12rem;
+    border-bottom-right-radius: 12rem;
+    justify-content: space-evenly;
+    background-color: rgb(255, 222, 172);
+
+    .band {
+      border: 1px solid black;
+      border-top: none;
+      border-bottom: none;
+      width: 1rem;
+
+      &:hover {
+        cursor: pointer;
+      }
+    }
+  }
+`
+
 const Band: FC<BandProps> = ({color = 'none'}) => (
   <div
     style={{backgroundColor: color === 'none' ? 'transparent' : color}}
-    className={styles.band}
+    className='band'
   />
 )
 
 const Resistor: FC<ResistorProps> = ({ bandA, bandB, bandC, bandD }) => {
 
   return (
-    <div className={styles.container}>
-      <div className={styles.resistor}>
+    <ResistorWrapper>
+      <div className='resistor'>
         <Band color={ bandA }/>
         <Band color={ bandB }/>
         <Band color={ bandC }/>
         <Band color={ bandD }/>
       </div>
-    </div>
+    </ResistorWrapper>
   )
 }
 
