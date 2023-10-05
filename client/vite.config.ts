83fcc6317583e1937ab3eb332bd24e31b61d59ed
Manuel Alejandro Rodriguez Gonzalez <manuelalejandrorodriguezgonzalez@Manuels-Mac-mini.local>
2023-10-05 10:24:03 -0600

    Updated configuration of all three packages to fix dev and build issues when importing the common package

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['common']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/common/, /node_modules/]
+    }
+  }
 })
